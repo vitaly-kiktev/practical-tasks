9315f7c9f760f4ecf217c9c28f9c49fa73220e83
i5t4b3r <i5t4b3r@gmail.com>
2024-12-24 14:05:59 +0300

    end: lesson 1 task 2 multiple char

--- src/com/walking/lesson1_varaibles/Task2MultipleChar.java
+++ src/com/walking/lesson1_varaibles/Task2MultipleChar.java
@@ -5,5 +5,11 @@ package com.walking.lesson1_varaibles;
  */
 public class Task2MultipleChar {
     public static void main(String[] args) {
+        char j = 'J';
+        char a = 'a';
+        char c = 'c';
+        char k = 'k';
+
+        System.out.println("" + j + a + c + k);
     }
 }
